2e562571c10e27075aaf22b70ce0139b8f9de733
Bhautik <bhautikkatariya773@gmail.com>
2024-12-31 14:13:28 +0530

    edited settings.py

--- project/settings.py
+++ project/settings.py
@@ -76,14 +76,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {  
-    'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'db_gallary',  
-        'USER': 'root',  
-        'PASSWORD': 'bhautik@12345',  
-        'HOST': '127.0.0.1',  
-        'PORT': '3306',
-    }  
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3', 
+    }
 }  
 
 # Password validation
