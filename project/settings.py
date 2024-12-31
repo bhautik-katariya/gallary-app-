a48fe77bd5c33dc2333e4012be8f7ae879cc55b0
Bhautik <bhautikkatariya773@gmail.com>
2024-12-31 13:51:21 +0530

    edited settings.py

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5za$w*+(tfafbdk^oho%nc_-%4(-a193=f8^l*rg63n6j7v50w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # AUTH_USER_MODEL = 'app.CustomUser'
 
